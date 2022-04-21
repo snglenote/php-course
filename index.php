a727cbcdf82201b9e19c23e42c079eae05832f36
Calvine Akoonyatse <cakoonyatse@gmail.com>
2022-04-21 23:58:08 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,12 +5,18 @@
   </head>
   <body>
     <?php
-    $name = "PHP Store";
-    $credit = 1000;
-    // Using two forward slashes we create comments in php.
-    // Within the php tag, we can insert values into our HTML.
-   echo "<h1>Welcome to ".$name."!</h1>";
-echo "<h2>You have $".$credit." in your wallet.</h2>";";
+      $name = "PHP Store";
+      $credit = 1000; 
+
+      echo "<h1>Welcome to ".$name."!</h1>";
+      echo "<h2>You have $".$credit." in your wallet.</h2>";
+
+	    $products['Computer']=750;
+	    $products['Car']=15000;
+	    $products['iPhone']=1000;
+	    $products['Toaster']=75;
+
+	    echo "<p>A car costs $".$products['Car']."</p>";
     ?>
   </body>
 </html>
